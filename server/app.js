0237546a3e6da91cc22955eb60af40a421ab2de1
TanmayNikam <tanmaynikam2002@gmail.com>
2021-09-24 22:38:04 +0530

    auth added

--- server/app.js
+++ server/app.js
@@ -8,11 +8,17 @@ const cors = require("cors")
 
 
 
-const uri = `mongodb+srv://xer_app2:${process.env.DB_PASSWORD}@cluster0.gialc.mongodb.net/ExerciseTracker?retryWrites=true&w=majority`
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6dtmr.mongodb.net/ExerciseTracker?retryWrites=true&w=majority`
 
 
-mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true, useCreateIndex:true},()=>{ 
-    console.log("Database connected successfully!");
+mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true},(error)=>{ 
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("Database connected successfully!");
+    }
+    
 })
 
 
@@ -24,22 +30,17 @@ app.use(cookieParser())
 app.use(cors())
 
 //routes middlewares 
-// const authRouter = require("./routes/auth.js");
-// const userRouter = require("./routes/user.js")
-// const categoryRoutes = require("./routes/category.js")
-// const productRoutes = require("./routes/product.js")
-// const paymentRoutes = require("./routes/payment.js")
-// app.use("/api",authRouter)
-// app.use("/api",userRouter)
-// app.use("/api",categoryRoutes)
-// app.use("/api",productRoutes)
-// app.use("/api",paymentRoutes)
+const authRouter = require("./routes/auth.js");
+const userRouter = require("./routes/user.js")
+
+app.use("/api",authRouter)
+app.use("/api",userRouter)
 
 app.get("/",(req,res)=>{
     res.end("chalu h loveday!");
 })
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port,()=>{
     console.log(`Server started and running on port ${port}`);
