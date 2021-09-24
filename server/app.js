f17c407e6b4e70e6923096bb66e1759b3ff51954
TanmayNikam <tanmaynikam2002@gmail.com>
2021-09-25 01:25:16 +0530

    food api

--- server/app.js
+++ server/app.js
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-
-const uri = `mongodb+srv://xer_app2:${process.env.DB_PASSWORD}@cluster0.gialc.mongodb.net/ExerciseTracker?retryWrites=true&w=majority`;
-
-mongoose.connect(
-	uri,
-	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-	() => {
-		console.log("Database connected successfully!");
-	}
-);
-=======
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -41,7 +22,6 @@ mongoose.connect(uri,{ useNewUrlParser: true,useUnifiedTopology: true},(error)=>
 })
 
 
->>>>>>> be380f72368fa7069d8ff2dce718d5e7e7accbdc
 
 //middlewares
 app.use(morgan("dev"));
@@ -49,26 +29,12 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
 
-<<<<<<< HEAD
-//routes middlewares
-// const authRouter = require("./routes/auth.js");
-// const userRouter = require("./routes/user.js")
-// const categoryRoutes = require("./routes/category.js")
-// const productRoutes = require("./routes/product.js")
-// const paymentRoutes = require("./routes/payment.js")
-// app.use("/api",authRouter)
-// app.use("/api",userRouter)
-// app.use("/api",categoryRoutes)
-// app.use("/api",productRoutes)
-// app.use("/api",paymentRoutes)
-=======
 //routes middlewares 
 const authRouter = require("./routes/auth.js");
 const userRouter = require("./routes/user.js")
 
 app.use("/api",authRouter)
 app.use("/api",userRouter)
->>>>>>> be380f72368fa7069d8ff2dce718d5e7e7accbdc
 
 app.get("/", (req, res) => {
 	res.end("chalu h loveday!");
