5162339f653789f27b17d8effb5a7655303973f7
shashank <shashankkumar2023@gmail.com>
2021-09-28 00:34:29 +0530

    navbar-updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,40 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import {
+	BrowserRouter,
+	BrowserRouter as Router,
+	Route,
+	Switch
+} from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/navbar";
 import Exercise from "./components/exercise";
+import Home from "./components/home";
+import Signin from "./components/signin";
+import Signup from "./components/signup";
 import Food from "./components/food";
 // import CreateExercise from "./components/create-exercise.component";
 import Userinfo from "./components/user";
 
 function App() {
 	return (
-		<Router>
-			<div className='container'>
-				<Navbar />
-				<Userinfo />
+		<BrowserRouter>
+			<Navbar />
+			<Switch>
+				<Route path='/' exact component={Home} />
+				<Route path='/signin' exact component={Signin} />
+				<Route path='/signup' exact component={Signup} />
 
-				<div className='container_body'>
-					<Exercise />
-					<Food />
-				</div>
+				{/* <PrivateRoute
+          path="/user/dashboard"
+          exact
+          component={Dashboard}
+          /> */}
 
-				{/* <br />
-				<Route path='/' exact component={ExercisesList} />
-				<Route path='/edit/:id' component={EditExercise} />
-				<Route path='/create' component={CreateExercise} />
-				<Route path='/user' component={CreateUser} /> */}
-			</div>
-		</Router>
+				<Route path='/exercise' exact component={Exercise} />
+				<Route path='/food' exact component={Food} />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,32 +1,92 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { FaBars } from "react-icons/fa";
+import { links, social } from "./data";
 import { Link } from "react-router-dom";
-const navbar = () => {
+import logo from "./logo.svg";
+import "./navbar.css";
+const Navbar = () => {
+	const [showLinks, setShowLinks] = useState(false);
+	const linksContainerRef = useRef(null);
+	const linksRef = useRef(null);
+	const toggleLinks = () => {
+		setShowLinks(!showLinks);
+	};
+	useEffect(() => {
+		const linksHeight = linksRef.current.getBoundingClientRect().height;
+		if (showLinks) {
+			linksContainerRef.current.style.height = `${linksHeight}px`;
+		} else {
+			linksContainerRef.current.style.height = "0px";
+		}
+	}, [showLinks]);
 	return (
-		<nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
-			<Link to='/' className='navbar-brand'>
-				ExcerTracker
-			</Link>
-			<div className='collpase navbar-collapse'>
-				<ul className='navbar-nav mr-auto'>
-					<li className='navbar-item'>
-						<Link to='/' className='nav-link'>
-							Food
-						</Link>
-					</li>
-					<li className='navbar-item'>
-						<Link to='/create' className='nav-link'>
-							Food Calorie Log
-						</Link>
-					</li>
-					<li className='navbar-item'>
-						<Link to='/user' className='nav-link'>
-							Create User
-						</Link>
-					</li>
+		<nav>
+			<div className='nav-center'>
+				<div className='nav-header'>
+					{/* <img src={logo} className='logo' alt='logo' /> */}
+					<div className='navbar-brand'>ExcerTracker </div>
+					<button className='nav-toggle' onClick={toggleLinks}>
+						<FaBars />
+					</button>
+				</div>
+				<div className='links-container' ref={linksContainerRef}>
+					<ul className='links' ref={linksRef}>
+						{links.map((link) => {
+							const { id, url, text } = link;
+							return (
+								<li key={id}>
+									<a href={url}>{text}</a>
+								</li>
+							);
+						})}
+					</ul>
+				</div>
+				<ul className='social-icons'>
+					{social.map((socialIcon) => {
+						const { id, url, icon } = socialIcon;
+						return (
+							<li key={id}>
+								<a href={url}>{icon}</a>
+							</li>
+						);
+					})}
 				</ul>
 			</div>
 		</nav>
 	);
 };
 
-export default navbar;
+export default Navbar;
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+// const navbar = () => {
+// 	return (
+// 		<nav className='navbar navbar-dark bg-dark navbar-expand-lg'>
+// 			<Link to='/' className='navbar-brand'>
+// 				ExcerTracker
+// 			</Link>
+// 			<div className='collpase navbar-collapse'>
+// 				<ul className='navbar-nav mr-auto'>
+// 					<li className='navbar-item'>
+// 						<Link to='/' className='nav-link'>
+// 							Food
+// 						</Link>
+// 					</li>
+// 					<li className='navbar-item'>
+// 						<Link to='/create' className='nav-link'>
+// 							Food Calorie Log
+// 						</Link>
+// 					</li>
+// 					<li className='navbar-item'>
+// 						<Link to='/user' className='nav-link'>
+// 							Create User
+// 						</Link>
+// 					</li>
+// 				</ul>
+// 			</div>
+// 		</nav>
+// 	);
+// };
+
+// export default navbar;
